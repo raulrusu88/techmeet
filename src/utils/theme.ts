e37b9004222e7e54b3a66a7f168f9da02cad3121
hazelnutx <raulrusu46@gmail.com>
2021-11-24 23:51:16 +0200

    chakra custom theme colors

---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    primary: "#0e2439",
+    secondary: "#1f364d",
+    lightblue: "#3ec7e0",
+    blue: "#526bf4",
+    pink: "#e052a0",
+    red: "#f15c41",
+  },
+};
+
+export const theme = extendTheme({ colors });
